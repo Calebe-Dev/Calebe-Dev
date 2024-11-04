92161c1173a9ecbb62479dec71e48cc562b32796
Calebe-Dve <calebe.araujo101@gmail.com>
2024-11-04 12:42:45 -0300

    First Commit, files of old repo

---
+++ README.md
@@ -0,0 +1,65 @@
+# 👋 Hi there! I'm Calebe
+
+Welcome to my GitHub profile! I'm a student of Systems Analysis and Development and a budding web developer, committed to creating practical applications and solutions that enhance user experience. Each project here reflects my dedication to continuous learning and hands-on development.
+
+---
+
+###  About Me
+
+💡 **Idea Explorer**: Passionate about technology and innovation, I'm always on the lookout for creative ways to solve real-world problems.
+
+🎓 **University Student**: Currently in my 3rd semester of Systems Analysis and Development, with a background in Web Design.
+
+🛠 **Developer in Training**: Working as a freelancer with a focus on web development, creating landing pages, and online menus, especially for small businesses.
+
+👨‍💻 **Experienced in OS**: Skilled in using macOS and Linux for development, utilizing their unique tools and features to enhance my productivity and development environment.
+
+---
+
+### 💼 Professional Experience
+
+- **Freelancer** - Web Developer | Web Designer
+  - Building websites and landing pages
+  - Setting up API integrations and workflow automation
+  - Supporting new businesses to establish a professional digital presence
+
+- **Internship Project** - Documentation and FAQ Organization
+  - Organized and structured website FAQ contents to improve user experience.
+
+---
+
+### 💻 Skills
+
+**Front-end**  
+![HTML](https://img.shields.io/badge/-HTML5-E34F26?style=flat-square&logo=html5&logoColor=white)
+![CSS](https://img.shields.io/badge/-CSS3-1572B6?style=flat-square&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/-JavaScript-F7DF1E?style=flat-square&logo=javascript&logoColor=black)
+![Angular](https://img.shields.io/badge/-Angular-DD0031?style=flat-square&logo=angular&logoColor=white)
+
+**Back-end**  
+![Node.js](https://img.shields.io/badge/-Node.js-339933?style=flat-square&logo=node.js&logoColor=white)
+
+**Others**  
+![Git](https://img.shields.io/badge/-Git-F05032?style=flat-square&logo=git&logoColor=white)
+![Elementor](https://img.shields.io/badge/-Elementor-92003B?style=flat-square&logo=elementor&logoColor=white)
+![macOS](https://img.shields.io/badge/-macOS-000000?style=flat-square&logo=apple&logoColor=white)
+![Linux](https://img.shields.io/badge/-Linux-FCC624?style=flat-square&logo=linux&logoColor=black)
+
+---
+
+### 🌱 Currently Learning
+
+I'm currently focusing on **Angular** and working to deepen my understanding of **data structures** to build more efficient applications.
+
+---
+w
+### 🌐 Connect With Me
+
+[![LinkedIn](https://img.shields.io/badge/-LinkedIn-blue?style=flat-square&logo=Linkedin&logoColor=white)](https://www.linkedin.com/in/calebe-dev)  
+✉️ **Email**: calebe.araujo101@gmail.com
+
+---
+
+### 🌟 GitHub Stats
+
+![Calebe's GitHub stats](https://github-readme-stats.vercel.app/api?username=Calebe-Dev&show_icons=true&theme=radical)
---
+++ src/scripts/scripts.js
@@ -0,0 +1,254 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const eraSelector = document.getElementById('era');
+    const modeToggleButton = document.getElementById('mode-toggle-button');
+    const themeLink = document.getElementById('theme-style');
+    const themeColorMeta = document.querySelector('meta[name="theme-color"]');
+    const dots = document.querySelectorAll('.dot');
+    const postSections = document.querySelectorAll('.post-section');
+    const sidebar = document.querySelector('.sidebar');
+    const sidebarToggle = document.getElementById('sidebar-toggle');
+    const prevButton = document.getElementById('prev-button');
+    const nextButton = document.getElementById('next-button');
+
+
+
+
+    const eraColors = {
+        modern: '#111',
+        '2020s': '#cor-2020s',
+        '2015s': '#cor-2015s',
+        '2010s': '#cor-2010s',
+        '2005s': '#cor-2005s',
+        '2005Dark': '#cor-2005sDark',
+        '2000s': '#cor-2000s',
+        '1995s': '#cor-1995s',
+        '1990s': '#cor-1990s',
+        '1985s': '#cor-1985s',
+        '1980s': '#cor-1980s',
+        '1975s': '#cor-1975s',
+        '1970s': '#cor-1970s'
+    };
+
+    function applyThemeColor(color) {
+        themeColorMeta.setAttribute('content', color);
+    }
+
+    if (eraSelector) {
+        eraSelector.addEventListener('change', () => {
+            const selectedEra = eraSelector.value;
+            let cssFile = '';
+            switch (selectedEra) {
+                case 'modern':
+                    cssFile = './src/styles/modern.css';
+                    break;
+                case '2020s':
+                    cssFile = './src/styles/2020s.css';
+                    break;
+                case '2015s':
+                    cssFile = './src/styles/2015s.css';
+                    break;
+                case '2010s':
+                    cssFile = './src/styles/2010s.css';
+                    break;
+                case '2005Dark':
+                    cssFile = './src/styles/2005sDark.css';
+                    break;
+                case '2005s':
+                    cssFile = './src/styles/2005s.css';
+                    break;
+                case '2000s':
+                    cssFile = './src/styles/2000s.css';
+                    break;
+                case '1995s':
+                    cssFile = './src/styles/1995s.css';
+                    break;
+                case '1990s':
+                    cssFile = './src/styles/1990s.css';
+                    break;
+                case '1985s':
+                    cssFile = './src/styles/1985s.css';
+                    break;
+                case '1980s':
+                    cssFile = './src/styles/1980s.css';
+                    break;
+                case '1975s':
+                    cssFile = './src/styles/1975s.css';
+                    break;
+                case '1970s':
+                    cssFile = './src/styles/1970s.css';
+                    break;
+                default:
+                    cssFile = './src/styles/modern.css';
+            }
+            themeLink.href = cssFile;
+            applyThemeColor(eraColors[selectedEra] || eraColors.modern);
+        });
+    }
+
+    if (modeToggleButton) {
+        modeToggleButton.addEventListener('click', () => {
+            document.body.classList.toggle('light-mode');
+            const isLightMode = document.body.classList.contains('light-mode');
+            themeLink.href = isLightMode ? './src/styles/light-theme.css' : './src/styles/dark-theme.css';
+        });
+    }
+
+    function animateProgressBars() {
+        const progressBars = document.querySelectorAll('.progress');
+        progressBars.forEach(bar => {
+            const targetWidth = bar.parentElement.dataset.skillLevel;
+            bar.style.width = targetWidth;
+        });
+    }
+
+    function applySlideAnimation(elements, delay = 0) {
+        elements.forEach((element, index) => {
+            setTimeout(() => {
+                element.style.opacity = '1';
+                element.style.transform = 'translateY(0)';
+            }, delay * (index + 1));
+        });
+    }
+
+    const typingAnimations = document.querySelectorAll('.typing-animation');
+    typingAnimations.forEach(element => {
+        const text = element.textContent;
+        element.textContent = '';
+        for (let i = 0; i < text.length; i++) {
+            const letterSpan = document.createElement('span');
+            letterSpan.textContent = text[i];
+            letterSpan.style.opacity = '0';
+            letterSpan.style.animationDelay = `${i * 0.1}s`;
+            letterSpan.style.animation = 'fadeIn 0.5s ease forwards';
+            element.appendChild(letterSpan);
+        }
+    });
+
+    window.addEventListener('load', () => {
+        animateProgressBars();
+
+        const projects = document.querySelectorAll('.project');
+        applySlideAnimation(projects, 200);
+    });
+
+    window.addEventListener('scroll', () => {
+        const aboutText = document.querySelector('.about-text');
+        if (isElementInViewport(aboutText)) {
+            aboutText.style.opacity = '1';
+        }
+    });
+
+    function isElementInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    if (postSections.length > 0 && dots.length > 0) {
+        postSections[0].classList.add('active');
+        dots[0].classList.add('active');
+    
+        dots.forEach((dot, index) => {
+            dot.addEventListener('click', () => {
+                postSections.forEach(section => {
+                    section.classList.remove('active');
+                });
+    
+                dots.forEach(d => {
+                    d.classList.remove('active');
+                });
+    
+                postSections[index].classList.add('active');
+                dot.classList.add('active');
+            });
+        });
+    }
+
+    if (sidebarToggle) {
+        sidebarToggle.addEventListener('click', () => {
+            sidebar.classList.toggle('hidden');
+        });
+    }
+
+    if (modeToggleButton) {
+        modeToggleButton.addEventListener('click', () => {
+            document.body.classList.toggle('light-mode');
+        });
+    }
+
+    let currentSectionIndex = 0;
+
+    function showSection(index) {
+        postSections.forEach((section, i) => {
+            section.classList.toggle('active', i === index);
+            dots[i].classList.toggle('active', i === index);
+        });
+    }
+
+    if (dots.length > 0) {
+        dots.forEach((dot, index) => {
+            dot.addEventListener('click', () => {
+                currentSectionIndex = index;
+                showSection(index);
+            });
+        });
+    }
+
+    if (prevButton) {
+        prevButton.addEventListener('click', () => {
+            if (currentSectionIndex > 0) {
+                currentSectionIndex -= 1;
+                showSection(currentSectionIndex);
+            }
+        });
+    }
+
+    if (nextButton) {
+        nextButton.addEventListener('click', () => {
+            if (currentSectionIndex < postSections.length - 1) {
+                currentSectionIndex += 1;
+                showSection(currentSectionIndex);
+            }
+        });
+    }
+
+    // Inicializa a primeira seção
+    showSection(currentSectionIndex);
+
+
+            // Função para abrir a janela de compartilhamento
+        function sharePost(platform, url, text) {
+            let shareUrl = '';
+            const encodedUrl = encodeURIComponent(url);
+            const encodedText = encodeURIComponent(text);
+
+            switch(platform) {
+                case 'twitter':
+                    shareUrl = `https://twitter.com/intent/tweet?url=${encodedUrl}&text=${encodedText}`;
+                    break;
+                case 'facebook':
+                    shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`;
+                    break;
+                case 'linkedin':
+                    shareUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodedUrl}`;
+                    break;
+            }
+
+            window.open(shareUrl, '_blank', 'width=600,height=400');
+        }
+
+        // Adiciona eventos aos botões de compartilhamento
+        document.querySelectorAll('.share-btn').forEach(button => {
+            button.addEventListener('click', () => {
+                const platform = button.getAttribute('data-platform');
+                const post = button.closest('.post');
+                const postTitle = post.querySelector('.post-header h2').innerText;
+                const postUrl = window.location.href + '#' + post.id;
+                sharePost(platform, postUrl, postTitle);
+            });
+        });
+});
