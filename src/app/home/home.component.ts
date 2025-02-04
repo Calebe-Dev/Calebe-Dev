c771be93db0a912269e668065d47a6a6ae222e1b
Calebe-Dev <calebe.araujo101@gmail.com>
2025-02-04 10:43:43 -0300

    Completando componentes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { HeroComponent } from "../hero/hero.component";
+import { AboutMeComponent } from "../about-me/about-me.component";
+import { SocialLinksComponent } from "../social-links/social-links.component";
+import { SkilsComponent } from "../skils/skils.component";
+import { ServicesComponent } from "../services/services.component";
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, RouterLinkActive, HeroComponent],
+  imports: [RouterLink, RouterLinkActive, HeroComponent, AboutMeComponent, SocialLinksComponent, SkilsComponent, ServicesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
