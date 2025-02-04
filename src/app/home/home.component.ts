48e43f748a37772513107e2921527ac4792348ca
Calebe-Dev <calebe.araujo101@gmail.com>
2025-02-04 15:43:33 -0300

    alterando meta dados e navbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,10 +5,11 @@ import { AboutMeComponent } from "../about-me/about-me.component";
 import { SocialLinksComponent } from "../social-links/social-links.component";
 import { SkilsComponent } from "../skils/skils.component";
 import { ServicesComponent } from "../services/services.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, RouterLinkActive, HeroComponent, AboutMeComponent, SocialLinksComponent, SkilsComponent, ServicesComponent],
+  imports: [RouterLink, RouterLinkActive, HeroComponent, AboutMeComponent, SocialLinksComponent, SkilsComponent, ServicesComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
