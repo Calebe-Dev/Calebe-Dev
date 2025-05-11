d7b6ced598b76ad79ed77a736678bab37d642bff
Calebe Araujo <144181633+Calebe-Dev@users.noreply.github.com>
2025-05-11 15:50:17 -0300

    feat: add main layout and component structure
    - Added main layout with smooth scrolling and full-height sections
    - Integrated multiple components (navbar, hero, about, skills, etc.) into the app
    - Updated package.json with new dependencies for animations, smooth scrolling, and UI enhancements
    - Initialized AOS library for scroll animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { HeroComponent } from "./components/hero/hero.component";
+import { TestimonialsComponent } from "./components/testimonials/testimonials.component";
+import { CallToActionComponent } from "./components/call-to-action/call-to-action.component";
+import { ProjectRequestFormComponent } from "./components/project-request-form/project-request-form.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { AboutComponent } from "./components/about/about.component";
+import { SkillsComponent } from "./components/skills/skills.component";
+import { ServicesComponent } from "./components/services/services.component";
+import { ExperienceComponent } from "./components/experience/experience.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { isPlatformBrowser } from '@angular/common';
+import AOS from 'aos';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeroComponent, TestimonialsComponent, CallToActionComponent, ProjectRequestFormComponent, ContactComponent, FooterComponent, NavbarComponent, AboutComponent, SkillsComponent, ServicesComponent, ExperienceComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Calebe-Dev';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init({
+        duration: 1000,
+        once: true,
+        easing: 'ease-out'
+      });
+    }
+  }
+
 }
